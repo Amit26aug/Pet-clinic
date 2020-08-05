fedc46368e07375fbec3feb9630693d40599bb38
amit.sharma@hitachivantara.com <alliswell420idiots>
2020-08-06 02:56:43 +0530

    Create Spring data JPA service for Vets: done and closes #41

---
+++ pet-clinic-data/src/main/java/com/august/mypetclinic/services/jpa/VetServiceJpa.java
@@ -0,0 +1,44 @@
+package com.august.mypetclinic.services.jpa;
+
+import com.august.mypetclinic.model.Vet;
+import com.august.mypetclinic.repositories.VetRepository;
+import com.august.mypetclinic.services.VetService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VetServiceJpa implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetServiceJpa(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Vet save(Vet vet) {
+        return vetRepository.save(vet);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet vet) {
+        vetRepository.delete(vet);
+    }
+}
